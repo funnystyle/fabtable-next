19ac948d75c75677963d09bd51b0bd767543308a
funnystyle <esero@hanmail.net>
2024-08-29 21:32:42 +0900

    MKP-23 DataTables serverside data

--- components/DataTables.js
+++ components/DataTables.js
@@ -5,42 +5,45 @@ import "datatables.net-responsive-dt";
 import "datatables.net-dt/css/dataTables.dataTables.min.css";
 import "datatables.net-responsive-dt/css/responsive.dataTables.min.css";
 
-const DataTables = ({ columns, data, url }) => {
-  const tableRef = useRef();
-
+const createDataTablesOptions = (columns, data, url) => {
   let dataTableOptions = {
     columns: columns,
     responsive: true, // 반응형 켜기
     // options
   };
 
-  if (url) {
-    const serverUrl = `${process.env.REACT_APP_API_BASE_URL}${url}`;
-
-    dataTableOptions = {
-      ...dataTableOptions,
-      serverSide: true,
-      processing: true,
-      paging: true,
-      ajax: {
-        contentType: "application/json",
-        url: serverUrl,
-        type: "POST",
-        data: function (d) {
-          d.order.forEach(function (o, i) {
-            o.columnName = d.columns[o.column].data;
-          }); // sort data
-          return JSON.stringify(d);
-        },
-      },
-    };
-  } else {
-    dataTableOptions = { ...dataTableOptions, data: data };
+  // data가 있으면 clientSide
+  if (data) {
+    return { ...dataTableOptions, data: data };
   }
 
-  useEffect(() => {
-    // $.springFriendly();
+  // url이 있으면 serverSide
+  const serverUrl = `${process.env.REACT_APP_API_BASE_URL}${url}`;
 
+  return {
+    ...dataTableOptions,
+    serverSide: true,
+    processing: true,
+    ajax: {
+      contentType: "application/json",
+      url: serverUrl,
+      type: "POST",
+      data: function (d) {
+        d.order.forEach(function (o, i) {
+          o.columnName = d.columns[o.column].data;
+        }); // sort data
+        return JSON.stringify(d);
+      },
+    },
+  };
+};
+
+const DataTables = ({ columns, data, url }) => {
+  const tableRef = useRef();
+
+  const dataTableOptions = createDataTablesOptions(columns, data, url);
+
+  useEffect(() => {
     const table = $(tableRef.current).DataTable(dataTableOptions);
 
     // 언마운트 시 destroy
--- components/JQueryFriendly.js
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-
-export default function JQueryFriendly() {
-  return (
-    <Head>
-      <script src="@/lib/jquery.spring-friendly.js"></script>
-    </Head>
-  );
-}
