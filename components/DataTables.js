adfc271c1c460a20dfbb45c2067c9c0d1fc3f243
Pyeong-an <pyeongankim5@gmail.com>
2024-09-03 14:04:02 +0900

    리팩토링

--- components/DataTables.js
+++ components/DataTables.js
@@ -1,17 +1,13 @@
-import { useEffect, useMemo, useRef } from "react";
+import { useEffect, useRef } from "react";
 import $ from "jquery";
 import "datatables.net-dt";
 import "datatables.net-responsive-dt";
-// import "datatables.net-dt/css/jquery.dataTables.min.css";
 import "datatables.net-dt/css/dataTables.dataTables.min.css";
 import "datatables.net-responsive-dt/css/responsive.dataTables.min.css";
 import { createDataTablesOptions } from "@components/DataTableOptions";
 
 const DataTables = ({ columns, data, url, page }) => {
-  const dataTableOptions = useMemo(
-    () => createDataTablesOptions(columns, data, url, page),
-    [columns, data, url, page]
-  );
+  const dataTableOptions = createDataTablesOptions(columns, data, url, page);
 
   const tableRef = useRef();
 
@@ -32,7 +28,7 @@ const DataTables = ({ columns, data, url, page }) => {
   };
 
   const getCurrentPage = (target) => {
-    return +$(target).closest("nav").find("button.current").text();
+    return +$(target).closest("nav").find("button.current").data("dt-idx") + 1;
   };
 
   const getButtonPage = (target) => {
@@ -55,37 +51,25 @@ const DataTables = ({ columns, data, url, page }) => {
     // 페이지 버튼 클릭 핸들러
     const handlePageClick = (e) => {
       const clickedPage = getClickedPage(e.target, table);
-
       const params = new URLSearchParams(window.location.search);
       params.set("page", clickedPage);
       const newUrl = `?${params.toString()}`;
       window.history.pushState("", "", newUrl);
     };
-
-    // 클릭 이벤트 바인딩
     $(document).on("click", "button.dt-paging-button", handlePageClick);
 
     // popstate 이벤트 핸들러
     const handlePopState = () => {
-      let page = 1;
-      const querystring = window.location.search;
-      const queries = querystring.substring(1).split("&"); // Remove '?' at the start
-      queries.forEach((q) => {
-        if (q.startsWith("page=")) {
-          const p = q.split("=")[1];
-          if (p) page = parseInt(p, 10);
-        }
-      });
-
+      const params = new URLSearchParams(window.location.search);
+      const page = +(params.get("page") || 1);
       table.page(page - 1).draw(false); // DataTable에서 특정 페이지로 이동
     };
-
     window.addEventListener("popstate", handlePopState);
 
     // 언마운트 시 destroy 및 이벤트 핸들러 제거
     return () => {
-      $(document).off("click", "button.dt-paging-button", handlePageClick);
       window.removeEventListener("popstate", handlePopState);
+      $(document).off("click", "button.dt-paging-button", handlePageClick);
       table.destroy();
     };
   }, []);
