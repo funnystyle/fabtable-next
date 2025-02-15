27de6ef0b63b31210a28c7d0e4564b7bd8f203a1
saerom.shin <pursue09@naver.com>
2025-02-16 03:56:13 +0900

    영업관리 작업중

--- pages/components/HeadMeta.js
+++ pages/components/HeadMeta.js
@@ -5,6 +5,7 @@ const HeadMeta = ({ title, description, url, image }) => {
 		<Head>
 			<title>{title || "MKPFT"}</title>
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+			{/* <meta name="viewport" content="width=device-width, initial-scale=1, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> */}
 		</Head>
 	);
 };
