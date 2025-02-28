232bc6bd4294647b6abe1e51335f9a8aca935887
Pyeong-an <pyeongankim5@gmail.com>
2025-02-28 17:12:10 +0900

    라디오 설정

--- components/inputForm/handleInputComponent.js
+++ components/inputForm/handleInputComponent.js
@@ -1,4 +1,4 @@
-import { DatePicker, Form, Input, Select } from "antd";
+import { DatePicker, Form, Input, Radio, Select } from "antd";
 import React, { useEffect } from "react";
 import { handleCodeListFilter } from "@components/inputForm/handleCodeListFilter";
 import { handleSelectChange } from "@components/inputForm/handleSelectChange";
@@ -38,9 +38,6 @@ export const handleInputComponent = (form, codeRelationSet, selectedCodes, setSe
       form.resetFields([recordColumn.name]);
     }
 
-    // (form, codeRelationSet, selectedCodes, setSelectedCodes, value, option)
-    const defaultOption = codeList.length === 1 ? codeList[0] : null;
-
     return (
       <Form.Item
         key={component.id}
@@ -78,6 +75,67 @@ export const handleInputComponent = (form, codeRelationSet, selectedCodes, setSe
       </Form.Item>
     );
   }
+
+  if (recordColumn.connectionDiv === 'CODE' && recordColumn.formDiv === 'RADIO') {
+    const codeList = handleCodeListFilter(selectedCodes, recordColumn);
+
+    codeRelationSet.add({ codeGroupId: recordColumn.codeGroupId, name: recordColumn.name });
+
+    if (codeList.length === 0) {
+      form.resetFields([recordColumn.name]);
+    }
+
+    return (
+      <Form.Item
+        key={component.id}
+        label={recordColumn.displayName}
+        name={recordColumn.name}
+      >
+        {codeList.length === 0 ? (
+          // ✅ codeList가 비어있을 때: 비활성화된 Radio.Group
+          <Radio.Group disabled>
+            <Radio value="">선택할 옵션이 없습니다.</Radio>
+          </Radio.Group>
+        ) : (
+          <Radio.Group
+            onChange={(e) => {
+              const selectedOption = codeList.find(option => option.codeName === e.target.value);
+              const option = {
+                value: selectedOption.codeName,
+                label: selectedOption.codeName,
+                'data-codegroup-id': recordColumn.codeGroupId,
+                'data-id': selectedOption.id,
+                'data-child-relations': JSON.stringify(selectedOption.childRelations),
+              };
+              handleSelectChange(
+                form,
+                codeRelationSet,
+                selectedCodes,
+                setSelectedCodes,
+                selectedOption.codeName, // ✅ 선택된 값
+                option // ✅ 선택된 객체 자체를 전달
+              );
+            }}
+            data-codegroup-id={recordColumn.codeGroupId}
+            defaultValue={codeList.length === 1 ? codeList[0].codeName : undefined}
+          >
+            {codeList.map(option => (
+              <Radio
+                key={option.id}
+                value={option.codeName}
+                data-codegroup-id={recordColumn.codeGroupId}
+                data-id={option.id}
+                data-child-relations={JSON.stringify(option.childRelations)}
+              >
+                {option.codeName}
+              </Radio>
+            ))}
+          </Radio.Group>
+        )}
+      </Form.Item>
+    );
+  }
+
 
 
   return null;
--- components/inputForm/handleSelectChange.js
+++ components/inputForm/handleSelectChange.js
@@ -1,13 +1,13 @@
 // 선택된 코드 정보를 갱신하는 함수
 export const handleSelectChange = (form, codeRelationSet, selectedCodes, setSelectedCodes, value, option) => {
 
-  // const codeGroupId = option['data-codegroup-id'] ?? option?.props['data-codegroup-id'];
-  // const codeId = option['data-id'] ?? option?.props['data-id'];  // codeId를 추출
-  // const childRelations = JSON.parse(option['data-child-relations'] ?? option?.props['data-child-relations']);  // childRelations를 추출
+  const codeGroupId = option['data-codegroup-id'] ?? option?.props['data-codegroup-id'];
+  const codeId = option['data-id'] ?? option?.props['data-id'];  // codeId를 추출
+  const childRelations = JSON.parse(option['data-child-relations'] ?? option?.props['data-child-relations']);  // childRelations를 추출
 
-  const codeGroupId = option?.props['data-codegroup-id'];
-  const codeId = option?.props['data-id'];  // codeId를 추출
-  const childRelations = JSON.parse(option?.props['data-child-relations']);  // childRelations를 추출
+  // const codeGroupId = option?.props['data-codegroup-id'];
+  // const codeId = option?.props['data-id'];  // codeId를 추출
+  // const childRelations = JSON.parse(option?.props['data-child-relations']);  // childRelations를 추출
 
   let newSelectedCodes = [...selectedCodes];
 
