82c3a9d01ee6393d9def4c603d20fb6806cda5e5
funnystyle <esero@hanmail.net>
2025-03-28 04:29:35 +0900

    수주, cs 목록 버튼영역 수정

--- pages/cs/list/index.js
+++ pages/cs/list/index.js
@@ -1,6 +1,6 @@
 // pages/order.js
 import React, { useEffect, useState } from "react";
-import { Layout, } from "antd";
+import { Button, Dropdown, Flex, Layout, Space, } from "antd";
 
 import DrawerComponent from "@publish/components/drawer";
 import { useQuery } from "@tanstack/react-query";
@@ -12,6 +12,93 @@ import CsListTable from "@components/cs/list/CsListTable";
 import useCsSearchModalStore from "@store/useCsSearchModalStore";
 import CsListSearchTags from "@components/cs/create/CsListSearchTags";
 import OrderListButtonArea from "@/components/order/list/OrderListButtonArea";
+
+import { DownOutlined, RedoOutlined } from "@ant-design/icons";
+
+const operationItems = [
+	{
+		label: "접수내용",
+		key: "1",
+	},
+	{
+		label: "진행내역",
+		key: "2",
+	},
+	{
+		label: "출장내역",
+		key: "3",
+	},
+	{
+		label: "후속조치",
+		key: "4",
+	},
+];
+
+const stateItems = [
+	{
+		label: "접수",
+		key: "1",
+	},
+	{
+		label: "진행",
+		key: "2",
+	},
+	{
+		label: "종결",
+		key: "3",
+	},
+	{
+		label: "취소",
+		key: "4",
+	},
+];
+
+const handleMenuClick = (e) => {
+	// message.info("Click on menu item.");
+	console.log("click", e);
+};
+
+const excelItems = [
+	{
+		label: "편집 항목만",
+		key: "1",
+		children: [
+			{
+				key: "1-1",
+				label: "선택한 행",
+			},
+			{
+				key: "1-2",
+				label: "전체 행",
+			},
+		],
+	},
+	{
+		label: "전체 항목",
+		key: "2",
+		children: [
+			{
+				key: "2-1",
+				label: "선택한 행",
+			},
+			{
+				key: "2-2",
+				label: "전체 행",
+			},
+		],
+	},
+];
+
+const printItems = [
+	{
+		label: "프린트",
+		key: "1",
+	},
+	{
+		label: "양식 다운로드",
+		key: "2",
+	},
+];
 
 const OrderComponent = ({ contentHeight }) => {
 
@@ -47,7 +134,78 @@ const OrderComponent = ({ contentHeight }) => {
 				<CsListSearchTags />
 
 				{/* 상단 버튼 */}
-				<OrderListButtonArea statusList={statusList} />
+				{/* <OrderListButtonArea statusList={statusList} /> */}
+				<Flex gap="small" align="center" className="btn-big" style={{
+								position: "sticky",
+								top: "0",
+								zIndex: "10",
+								paddingBottom: "12px",
+								paddingTop: "8px",
+								backgroundColor: "#FFF",
+							}}>
+							<Button
+								variant="outlined"
+								icon={<RedoOutlined />}
+								className="icon-redo"
+							>
+								전체 목록
+							</Button>
+
+							<Flex gap="small" className="btn-spacing-area">
+								<Button variant="outlined">수주 종합정보</Button>
+
+								<Button variant="outlined">C/S 이력</Button>
+
+								<Dropdown
+									menu={{ items: stateItems, onClick: handleMenuClick }}
+								>
+									<Button>
+										<Space>
+											상태별 보기
+											<DownOutlined />
+										</Space>
+									</Button>
+								</Dropdown>
+
+								<Dropdown
+									menu={{ items: operationItems, onClick: handleMenuClick }}
+								>
+									<Button>
+										<Space>
+											구분별 보기
+											<DownOutlined />
+										</Space>
+									</Button>
+								</Dropdown>
+							</Flex>
+
+							<Flex gap="small" className="btn-spacing-area">
+								<Button>C/S 복제하기</Button>
+
+								<Button>항목편집</Button>
+
+								<Dropdown
+									menu={{ items: excelItems, onClick: handleMenuClick }}
+									className="excel-menu"
+								>
+									<Button>
+										<Space>
+											엑셀 다운로드
+											<DownOutlined />
+										</Space>
+									</Button>
+								</Dropdown>
+
+								<Dropdown menu={{ items: printItems }}>
+									<Button>
+										<Space>
+											출력하기
+											<DownOutlined />
+										</Space>
+									</Button>
+								</Dropdown>
+							</Flex>
+						</Flex>
 
 				{/* 태그 없음, 헤더 관련 정리 event */}
 				<CsListTable contentHeight={contentHeight} />
