a32008130f2b2238fd56fb167b8488e7c27f215c
funnystyle <esero@hanmail.net>
2025-04-01 14:07:02 +0900

    C/S 목록 우클릭 메뉴 수정

--- components/cs/list/data/csListRightItem.js
+++ components/cs/list/data/csListRightItem.js
@@ -26,55 +26,63 @@ export const csListRightItem = [
 		key: "2",
 	},
 	{
-		label: "수주 종합정보",
+		label: "C/S 이력",
 		key: "3",
-		disabled: true,
+	},
+	{
+		label: "C/S 이력",
+		key: "4",
+	},
+	{
+		label: "수주 종합정보",
+		key: "5",
+		// disabled: true,
 	},
 	{
 		type: "divider",
 	},
 	{
 		label: "출력하기",
-		key: "4",
+		key: "6",
 		children: [
 			{
-				key: "4-1",
+				key: "6-1",
 				label: "프린트",
 			},
 			{
-				key: "4-2",
+				key: "6-2",
 				label: "양식다운로드",
 			},
 		],
 	},
 	{
 		label: "엑셀 다운로드",
-		key: "5",
+		key: "7",
 		children: [
 			{
-				key: "5-1",
+				key: "7-1",
 				label: "편집 항목만",
 				children: [
 					{
-						key: "5-1-1",
+						key: "7-1-1",
 						label: "선택한 행",
 					},
 					{
-						key: "5-1-2",
+						key: "7-1-2",
 						label: "전체 행",
 					},
 				],
 			},
 			{
-				key: "5-2",
+				key: "7-2",
 				label: "전체 항목",
 				children: [
 					{
-						key: "5-2-1",
+						key: "7-2-1",
 						label: "선택한 행",
 					},
 					{
-						key: "5-2-2",
+						key: "7-2-2",
 						label: "전체 행",
 					},
 				],
@@ -86,6 +94,6 @@ export const csListRightItem = [
 	},
 	{
 		label: "삭제하기",
-		key: "7",
+		key: "8",
 	},
 ];
