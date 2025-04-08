a7de6c97c48a023207b2ea0f7108c9d7202a2b22
funnystyle <esero@hanmail.net>
2025-04-08 16:53:34 +0900

    MKP-182 [FE] 수주현황 우클릭 메뉴 변경

--- components/order/list/data/orderListRightItem.js
+++ components/order/list/data/orderListRightItem.js
@@ -1,65 +1,87 @@
 export const orderListRightItem = [
 	{
-		label: "수주 복제하기",
+		label: "수주 종합정보",
 		key: "1",
 	},
 	{
-		label: "수주 일괄수정",
+		label: "수주 복제하기",
 		key: "2",
 	},
 	{
-		label: "수주 종합정보",
+		label: "수주 일괄수정",
 		key: "3",
 	},
 	{
-		label: "부서별 메모",
+		label: "수주 상태변경",
 		key: "4",
+		children: [
+			{
+				key: "4-1",
+				label: "입고완료",
+			},
+			{
+				key: "4-2",
+				label: "납품완료",
+			},
+			{
+				key: "4-3",
+				label: "반출대기",
+			},
+			{
+				key: "4-2",
+				label: "반출완료",
+			},
+		],
+	},
+	{
+		label: "부서별 메모",
+		key: "5",
 	},
 	{
 		type: "divider",
 	},
 	{
 		label: "인쇄하기",
-		key: "5",
+		key: "6",
 		children: [
 			{
-				key: "5-1",
+				key: "6-1",
 				label: "라벨 인쇄",
 			},
 			{
-				key: "5-2",
+				key: "6-2",
 				label: "성적서 인쇄",
 			},
 		],
 	},
 	{
 		label: "엑셀 다운로드",
-		key: "6",
+		key: "7",
 		children: [
 			{
-				key: "6-1",
+				key: "7-1",
 				label: "편집 항목만",
 				children: [
 					{
-						key: "6-1-1",
+						key: "7-1-1",
 						label: "선택한 행",
 					},
 					{
-						key: "6-1-2",
+						key: "7-1-2",
 						label: "전체 행",
 					},
 				],
 			},
 			{
-				key: "6-2",
+				key: "7-2",
 				label: "전체 항목",
 				children: [
 					{
-						key: "6-2-1",
+						key: "7-2-1",
 						label: "선택한 행",
 					},
 					{
-						key: "6-2-2",
+						key: "7-2-2",
 						label: "전체 행",
 					},
 				],
@@ -71,6 +93,6 @@ export const orderListRightItem = [
 	},
 	{
 		label: "삭제하기",
-		key: "7",
+		key: "8",
 	},
 ];
